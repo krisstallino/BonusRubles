29a1eeb5c034317a1ea0d2405b35f4e80077940f
krisstallino <krisstallino@gmail.com>
2021-06-21 18:32:22 +0300

    added Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int refill = 999;
+        int bonus = (refill) / 100;
+        if (refill > 1000) {
+            System.out.println("Баланс вашего счета: " + (balance + refill + bonus) + " руб");
+            System.out.println("Включая " + bonus + " бонусных рублей");
+        } else {
+            System.out.println("Баланс вашего счета: " + (balance + refill) + " руб.");
+        }
+
+    }
+}
