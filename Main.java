022336d93f9efea29057be98fb8a68b79504053f
krisstallino <krisstallino@gmail.com>
2021-06-21 18:41:19 +0300

    added Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int refill = 999;
+        int refill = 2150;
         int bonus = (refill) / 100;
         if (refill > 1000) {
             System.out.println("Баланс вашего счета: " + (balance + refill + bonus) + " руб");
